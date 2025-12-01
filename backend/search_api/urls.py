3f96c7bdb2a3da25cf0c3408a6bc48a8d726ee68
Khalid Lakbir <khalidlakbir5@gmail.com>
2025-12-01 23:50:32 +0100

    text search with ia

--- backend/search_api/urls.py
+++ backend/search_api/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('recipes/', views.get_all_recipes, name='get_all_recipes'),
     path('search/', views.search_recipes, name='search_recipes'),
     path('recipes/<int:recipe_id>/', views.get_recipe_details, name='get_recipe_details'),
+
+
 ]
